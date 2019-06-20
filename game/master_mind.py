b93da338a3e26115d68e4a6f5f98d9149071050d
Alejandro <jose.alejandro.her.ros@gmail.com>
2019-06-21 00:18:30 +0200

    Integration basic validation

--- game/master_mind.py
+++ game/master_mind.py
@@ -6,3 +6,23 @@ base_color = ['R', 'B', 'Y', 'G', 'W', 'O']
 attempts = 0
 can_play = True
 max_attempts = 10
+while can_play:
+    correct_color = []
+    color_code = ''
+    guessed_color = []
+    # Convert input to uppercase
+    print(
+        'Available values {}'.format(
+            ', '.join(base_color)
+        )
+    )
+    inpunt_value = input().upper()
+    # Convert String to List
+    inpunt_value = [x for x in inpunt_value]
+    attempts += 1
+    # create random base of colors
+    random_choise = random.sample(base_color, max_chips)
+    if len(random_choise) != len(inpunt_value):
+        print('Only four values ​​can be added')
+        continue
+
